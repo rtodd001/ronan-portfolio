7db29d4908519aa64498009893675f28559547d3
rtodd <rtodd001@ucr.edu>
2023-06-19 15:55:34 -0700

    Changed the scroll bar to carousel using state management

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,34 +1,44 @@
-import React from 'react'
-import { motion } from 'framer-motion'
+import React, { useCallback } from 'react'
 import { Project } from '@/typings'
 import ProjectCard from './ProjectCards'
+import useEmblaCarousel from 'embla-carousel-react'
+import { Button } from '@mui/material'
 type Props = {
     projects: Project[]
 }
 
 function Projects({projects}: Props) {
 
+  const [emblaRef, emblaApi] = useEmblaCarousel()
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext()
+  }, [emblaApi])
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev()
+  }, [emblaApi])
+
   return (
-    <motion.div 
-    className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row
-                maw-w-full px-10 justify-evenly mx-auto items-center'>
+    <div 
+    className='h-screen flex relative flex-col text-left md:flex-row
+                  maw-w-full px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Projects
         </h3>
 
-        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory
-                       scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A] z-20'>
-            {[projects?.map((project) => (
-                <ProjectCard key={project._id} project={project}/>
-            ))]}
-            {/** projects */}
-            {/** projects */}
-        </div>
-
-        <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0  h-[500px] -skew-y-12'>
+        <div  className='embla_viewport overflow-hidden' ref={emblaRef} >
+          <div className='w-full flex space-x-5 p-10 snap-x snap-mandatory'>
+              {[projects?.map((project) => (
+                  <ProjectCard key={project._id} project={project}/>
+              ))]}
+          </div>
 
         </div>
-    </motion.div>
+        <Button className='absolute left-8' onClick={scrollPrev} variant='contained' color='success'>{"<"}</Button>
+        <Button className='absolute right-8' onClick={scrollNext} variant='contained' color='success'>{">"}</Button>
+
+    </div>
   )
 }
 
--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -1,32 +1,41 @@
-import React from 'react'
+import React, { useCallback} from 'react'
 import ExperienceCard from './ExperienceCard'
-import { motion } from 'framer-motion'
 import { Experience } from '@/typings'
-
+import useEmblaCarousel from 'embla-carousel-react'
+import { Button } from '@mui/material'
 type Props = {
   experiences: Experience[];
 }
 
 function WorkExperience({ experiences }: Props) {
+
+  const [emblaRef, emblaApi] = useEmblaCarousel()
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext()
+  }, [emblaApi])
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev()
+  }, [emblaApi])
+
   return (
-    <motion.div 
-      initial={{opacity:0}}
-      whileInView={{opacity: 1}}
-      transition={{duration:1.2}}
-      className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row
+    <div
+      className='h-screen flex relative flex-col text-left md:flex-row
                     maw-w-full px-10 justify-evenly mx-auto items-center'>
       <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Experience
       </h3>
-
-      <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory
-                       scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]'>
-        {experiences?.map(experience => (
-          <ExperienceCard key={experience._id} experience={experience} />
-        ))}
-
+      <div className='embla_viewport overflow-hidden' ref={emblaRef} >
+        <div className='w-full flex space-x-5 p-10 snap-x snap-mandatory'>
+          {experiences?.map(experience => (
+            <ExperienceCard key={experience._id} experience={experience} />
+          ))}
+        </div>
       </div>
-    </motion.div>
+      <Button className='absolute left-8' onClick={scrollPrev} variant='contained' color='success'>{"<"}</Button>
+      <Button className='absolute right-8' onClick={scrollNext} variant='contained' color='success'>{">"}</Button>
+    </div>
   )
 }
 
